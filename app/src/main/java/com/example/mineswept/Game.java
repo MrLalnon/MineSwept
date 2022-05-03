deab114fe0ed2556cbcc81ad862af59a0d4d9ef0
Nir Yanke <nir.yankelovichh@gmail.com>
2022-05-03 13:09:39 +0300

    Starting to add Database

---
+++ app/src/main/java/com/example/mineswept/Game.java
@@ -0,0 +1,17 @@
+package com.example.mineswept;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class Game{
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public Date date;
+    public int Size;
+    public int Population;
+    public int time;
+}
